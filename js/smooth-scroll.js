653fa25eeb47a7b7a73b886ba51ed9adbef15656
JeremySavarin <jeremy.savarin@gmail.com>
2016-05-24 23:57:21 -0400

    Updated main page with pictures. Added new e-board members.

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -8,7 +8,14 @@ $("#navbar ul li a[href^='#']").on('click', function(event) {
     var hash = this.hash;
 
     $("html, body").animate({
-        scrollTop: $(hash).offset().top}, 500, function() {
-            window.location.hash = hash;
-    });
+        scrollTop: $(hash).offset().top},
+        'slow');
+});
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 699) {
+        $('.navbar').addClass('opaque');
+    } else {
+        $('.navbar').removeClass('opaque');
+    }
 });
